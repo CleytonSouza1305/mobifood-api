78c981ec9fd2442ca4386feeb4cc25755681180d
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-09-09 11:57:14 -0300

    Importando o arquivo de rota de usuário

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
 import { errorHandler } from './middleware/error-handler'
+import userRouter from './routers/user-router'
 
 dotenv.config()
 
@@ -10,6 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(errorHandler)
+app.use('/auth', userRouter)
 
 const PORT = process.env.PORT || 1000
 
