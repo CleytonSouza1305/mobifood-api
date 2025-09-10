ae35591f127d6d46e02111e8ba145c2732f2ff55
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-09-10 12:42:56 -0300

    Criando a função que busca um usuário por email e a função para criar um novo usuário

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,32 @@
 import { prisma } from "../database"
+import { UserRole } from "../generated/prisma"
 
 export class User {
   static allUsers = async () => {
      const users = await prisma.user.findMany()
 
      return users
+  }
+
+  static findByEmail = async (email:string) => {
+    const user = await prisma.user.findUnique({
+      where: { email}
+    })
+
+    return user
+  }
+
+  static createUser = async (username:string, email:string, password:string, phone:string, role:UserRole) => {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+        phone,
+        role
+      }
+    })
+
+    return newUser
   }
 }
