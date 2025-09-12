6005dde19f7d12569350f82813aa122c0dc9997f
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-09-12 11:46:42 -0300

    Criando método que retorna um usuário baseado no parâmetro "id" enviado

--- src/models/User.ts
+++ src/models/User.ts
@@ -16,6 +16,24 @@ export class User {
     return user
   }
 
+  static findById = async (id:number) => {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        phone: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+        addresses: true
+      }
+    })
+
+    return user
+  }
+
   static createUser = async (username:string, email:string, password:string, phone:string, role:UserRole) => {
     const newUser = await prisma.user.create({
       data: {
