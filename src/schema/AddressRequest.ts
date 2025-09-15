7b5ac4a1ceb652a80ef40fa709e4fe9d6f7ddbdc
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-09-15 20:05:31 -0300

    Criando o schema para validar a criação de um novo address

---
+++ src/schema/AddressRequest.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+
+const stateEnum = z.enum([
+  "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO",
+  "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI",
+  "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+]);
+
+const addressRole = z.enum(["Casa", "Trabalho", "Escola", "Outros"])
+
+const stateSchema = z.string().transform((s) => s.toUpperCase()).pipe(stateEnum);
+
+export const CreateAddressRequestSchema = z.object({
+  street: z.string().min(2).max(100),
+  city: z.string().min(2).max(100),
+  number: z.number(),
+  state: stateSchema,
+  role: addressRole.optional().default('Casa'),
+  isActive: z.boolean().optional()
+})
