e9dc92437b30dd2fa517b750b33ce895acd3f9be
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-09-18 12:50:04 -0300

    Importando o schema que valida os campos para atualizar a senha

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -3,6 +3,7 @@ import { User } from "../models/User";
 import {
   CreateUserRequestSchema,
   LoginRequestSchema,
+  UpdateUserPasswordRequestSchema,
   UpdateUserRequestSchema,
 } from "../schema/UserRequest";
 import { HttpError } from "../error/HttpError";
@@ -236,42 +237,57 @@ const changePassword: Handler = async (req, res, next) => {
       throw new HttpError(403, "Acesso negado.");
     }
 
-    const body = UpdateUserRequestSchema.parse(req.body)
+    const body = UpdateUserPasswordRequestSchema.parse(req.body);
     if (!body.password) {
-      throw new HttpError(400, 'É necessário informar a senha para continuar.')
+      throw new HttpError(400, "É necessário informar a senha para continuar.");
     }
 
     if (!body.newPassword) {
-      throw new HttpError(400, 'É necessário informar a nova senha para continuar.')
+      throw new HttpError(
+        400,
+        "É necessário informar a nova senha para continuar."
+      );
     }
 
-    const hashedPassword = await prisma.user.findUnique({ 
+    const hashedPassword = await prisma.user.findUnique({
       where: { id },
-      select: { password: true }
-    })
+      select: { password: true },
+    });
 
     if (!hashedPassword) {
-      throw new HttpError(500, 'Erro interno, tente novamente mais tarde')
+      throw new HttpError(500, "Erro interno, tente novamente mais tarde");
     }
 
-    const verifyPassword = bcrypt.compareSync(body.password, hashedPassword.password)
+    const verifyPassword = bcrypt.compareSync(
+      body.password,
+      hashedPassword.password
+    );
     if (!verifyPassword) {
-      throw new HttpError(400, 'Credenciais inválidas')
+      throw new HttpError(400, "Credenciais inválidas");
     }
 
-    const newPasswordHashed = bcrypt.hashSync(body.newPassword, 10)
-    const newPassword = await User.updateUser({ password: newPasswordHashed }, id)
-    res.json(newPassword)
+    const newPasswordHashed = bcrypt.hashSync(body.newPassword, 10);
+    const newPassword = await User.updateUser(
+      { password: newPasswordHashed },
+      id
+    );
+    res.json(newPassword);
   } catch (error) {
     if (error instanceof ZodError) {
       const errorFIeld = error.issues.map((el) => el.path.join(".")).join(", ");
 
-      if (errorFIeld.includes('password')) {
-        throw new HttpError(400, 'Formato de senha inválido, mínimo de 6 caracteres.')
+      if (errorFIeld.includes("password")) {
+        throw new HttpError(
+          400,
+          "Formato de senha inválido, mínimo de 6 caracteres."
+        );
       }
 
-      if (errorFIeld.includes('newPassword')) {
-        throw new HttpError(400, 'Formato ds novs senha inválido, mínimo de 6 caracteres.')
+      if (errorFIeld.includes("newPassword")) {
+        throw new HttpError(
+          400,
+          "Formato ds novs senha inválido, mínimo de 6 caracteres."
+        );
       }
     } else {
       next(error);
