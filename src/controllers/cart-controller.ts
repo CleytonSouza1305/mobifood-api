9edaac2897a1be7d137047f5ace6d3e696f98486
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-12-08 18:37:39 -0300

    Criando a rota e o controller para adicionar um item ao carrinho

--- src/controllers/cart-controller.ts
+++ src/controllers/cart-controller.ts
@@ -2,6 +2,8 @@ import { Handler } from "express";
 import { Cart } from "../models/Cart";
 import { HttpError } from "../error/HttpError";
 import { JwtPayload } from "jsonwebtoken";
+import { AddItemToCartRequest } from "../schema/CartRequest";
+import { ZodError } from "zod";
 
 // GET /api/cart
 const getCart: Handler = async (req, res, next) => {
@@ -31,9 +33,46 @@ const getCart: Handler = async (req, res, next) => {
 // POST /api/cart/add
 const addItemToCart: Handler = async (req, res, next) => {
   try {
-    
+    const body = AddItemToCartRequest.parse(req.body)
+
+    const cart = await Cart.cartById(body.cartId);
+    if (!cart) {
+      throw new HttpError(404, "Cart not found.");
+    }
+
+    if (!req.user || typeof req.user !== "object" || !("id" in req.user)) {
+      throw new HttpError(401, "Usuário não autenticado.");
+    }
+
+    const user = req.user as JwtPayload & { id: number; role: string };
+    if (user.id !== cart.userId && user.role !== "admin") {
+      throw new HttpError(403, "User not authorized.");
+    }
+
+    const itemAdded = await Cart.addItemToCart(body.cartId, body.itemId, body.quantity)
+    res.status(201).json(itemAdded)
+
+    res.json(true)
+
   } catch (error) {
-    next(error)
+    if (error instanceof ZodError) {
+          const errorFIeld = error.issues.map((el) => el.path.join(".")).join(", ");
+          if (errorFIeld.includes('cartId')) {
+            throw new HttpError(400, 'Cart id is required.')
+          } 
+
+          if (errorFIeld.includes('itemId')) {
+            throw new HttpError(400, 'Item id is required.')
+          } 
+
+          if (errorFIeld.includes('quantity')) {
+            throw new HttpError(400, 'The quantity is required.')
+          } 
+
+          res.json(errorFIeld)
+        } else {
+          next(error);
+        }
   }
 };
 
--- src/routers/cart-router.ts
+++ src/routers/cart-router.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/auth-user'
-import { getCart } from '../controllers/cart-controller'
+import { addItemToCart, getCart } from '../controllers/cart-controller'
 
 const cartRouter = express.Router()
 
 cartRouter.get('/cart/:id', authMiddleware, getCart)
-cartRouter.post('/cart/add', authMiddleware)
+cartRouter.post('/cart/add', authMiddleware, addItemToCart)
 
 export default cartRouter
