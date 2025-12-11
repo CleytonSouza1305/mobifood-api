5a960574b205c3aac9540dd21d4f4cf7a3aa8520
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-12-11 19:51:34 -0300

    Criando a função de atualizar o item dentro do cart

--- src/models/Cart.ts
+++ src/models/Cart.ts
@@ -44,7 +44,7 @@ export class Cart {
         },
       });
     } else {
-      await prisma.cartItem.createMany({
+      await prisma.cartItem.create({
         data: {
           cartId,
           itemId,
@@ -74,10 +74,56 @@ export class Cart {
       include: {
         items: {
           include: {
-            item: true
-          }
-        }
-      }
+            item: true,
+          },
+        },
+      },
+    });
+  };
+
+  static updateItemCart = async (
+    cartId: number,
+    itemId: number,
+    quantity: number
+  ) => {
+    const product = await prisma.products.findUnique({ where: { id: itemId } });
+    if (!product) {
+      return { message: `Product not found.` };
+    }
+
+    const cart = await prisma.cart.findUnique({
+      where: { id: cartId },
+      include: {
+        items: {
+          include: { item: true },
+        },
+      },
+    });
+
+    const existsItem = cart?.items.find((i) => i.itemId === product.id);
+    if (!existsItem) return { message: "Item not found in cart." }
+
+    await prisma.cartItem.update({
+      where: { id: existsItem.id },
+      data: {
+        quantity,
+        subTotal: +product.price * quantity,
+      },
+    });
+
+    const updatedCart = await prisma.cart.findUnique({
+      where: { id: cartId },
+      include: { items: true },
+    });
+
+    const total = updatedCart?.items.reduce(
+      (acc, item) => acc + (item.subTotal ?? 0),
+      0
+    );
+
+    await prisma.cart.update({
+      where: { id: cartId },
+      data: { total },
     });
   };
 }
