5596e66b569eb0d510de0acca30f72a69cb9633f
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-01-08 14:50:12 -0300

    Criando a rota para buscar no google maps a localização atual do usuário

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,25 +1,74 @@
 import express from "express";
 
 const userRouter = express.Router();
-import { getAllUsers, register, login, me, getUserById, updateUserById, deleteUserById, changePassword } from "../controllers/user-controller";
+import {
+  getAllUsers,
+  register,
+  login,
+  me,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  changePassword,
+} from "../controllers/user-controller";
 import { authMiddleware } from "../middleware/auth-user";
-import { listUserAddresses, createAddress, addressById, deleteAddress, updateAddress } from "../controllers/address-controller";
+import {
+  listUserAddresses,
+  createAddress,
+  addressById,
+  deleteAddress,
+  updateAddress,
+} from "../controllers/address-controller";
+import { HttpError } from "../error/HttpError";
 
 // USER
-userRouter.get('/users', getAllUsers);
-userRouter.post('/users/register', register);
-userRouter.post('/users/login', login);
-userRouter.get('/users/me', authMiddleware, me);
-userRouter.get('/users/:id', authMiddleware, getUserById);
-userRouter.put('/users/:id', authMiddleware, updateUserById);
-userRouter.delete('/users/:id', authMiddleware, deleteUserById);
-userRouter.put('/users/:id/change-password', authMiddleware, changePassword);
+userRouter.get("/users", getAllUsers);
+userRouter.post("/users/register", register);
+userRouter.post("/users/login", login);
+userRouter.get("/users/me", authMiddleware, me);
+userRouter.get("/users/:id", authMiddleware, getUserById);
+userRouter.put("/users/:id", authMiddleware, updateUserById);
+userRouter.delete("/users/:id", authMiddleware, deleteUserById);
+userRouter.put("/users/:id/change-password", authMiddleware, changePassword);
 
 // ADDRESS
-userRouter.get('/users/:id/addresses',authMiddleware, listUserAddresses);
-userRouter.post('/users/:id/address',authMiddleware, createAddress);
-userRouter.get('/users/:id/address/:addressId',authMiddleware, addressById);
-userRouter.delete('/users/:id/address/:addressId',authMiddleware, deleteAddress);
-userRouter.put('/users/:id/address/:addressId',authMiddleware, updateAddress);
+userRouter.get("/users/:id/addresses", authMiddleware, listUserAddresses);
+userRouter.post("/users/:id/address", authMiddleware, createAddress);
+userRouter.get("/users/:id/address/:addressId", authMiddleware, addressById);
+userRouter.delete(
+  "/users/:id/address/:addressId",
+  authMiddleware,
+  deleteAddress
+);
+userRouter.put("/users/:id/address/:addressId", authMiddleware, updateAddress);
+
+// Location
+
+userRouter.post('/reverse-geocoding', async (req, res, next) => {
+  const { latitude, longitude } = req.body;
+  const GOOGLE_API_KEY = 'SUA_CHAVE_AQUI'; 
+
+  try {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.status === 'OK') {
+      console.log(data)
+      const formattedAddress = data.results[0].formatted_address;
+
+      res.json({
+        display_name: formattedAddress,
+        details: data.results[0].address_components
+      });
+    } else {
+      throw new HttpError(404, "Google não encontrou o endereço.")
+    }
+
+  } catch (error) {
+    next(error)
+  }
+});
 
 export default userRouter;
