e87e8431c1d8aa535cc97791caa020741c3e10a2
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-01-15 18:20:53 -0300

    Criando e validando o controller de criação de order

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -2,8 +2,13 @@ import { Handler } from "express";
 import { User } from "../models/User";
 import { HttpError } from "../error/HttpError";
 import { Order } from "../models/Order";
+import { CreateOrderRequestSchema } from "../schema/OrderRequest";
+import { ZodError } from "zod";
+import { Cart } from "../models/Cart";
+import { Coupon } from "../models/Coupon";
+import calculateDiscount from "../utils/calculateDiscount";
 
-// order/user/:id
+// /api/order/user/:id
 const ordersByUserId: Handler = async (req, res, next) => {
   try {
     const userId = Number(req.params.id);
@@ -19,11 +24,77 @@ const ordersByUserId: Handler = async (req, res, next) => {
       throw new HttpError(403, "Busca inválida, você não tem permissão.");
     }
 
-    const orders = await Order.ordersByUserId(userId)
-    res.json(orders)
+    const orders = await Order.ordersByUserId(userId);
+    res.json(orders);
   } catch (error) {
     next(error);
   }
 };
 
-export { ordersByUserId };
+// POST /api/order
+const createOrder: Handler = async (req, res, next) => {
+  try {
+    const body = CreateOrderRequestSchema.parse(req.body);
+    const deliveryTax = 15;
+
+    if (!req.user || typeof req.user !== "object" || !("id" in req.user)) {
+      throw new HttpError(401, "Usuário não autenticado.");
+    }
+
+    const user = await User.findById(req.user.id);
+    if (!user) throw new HttpError(401, "Usuário não cadastrado.");
+
+    const cart = await Cart.cartById(Number(user.cart?.id));
+    if (!cart || cart.items.length <= 0) {
+      throw new HttpError(400, "Carrinho vazio ou não encontrado.");
+    }
+    
+    let totalOriginal = cart.total;
+    let totalDiscounted = cart.total; 
+    let deliveryFee = deliveryTax;
+
+    if (body.couponCode) {
+      const isValidCoupon = await Coupon.validadeCouponCode(body.couponCode);
+
+      if (!isValidCoupon) {
+        throw new HttpError(404, "Cupom inválido ou expirado.");
+      }
+
+      const discountPrice = calculateDiscount(
+        cart.total,
+        isValidCoupon.discountType,
+        isValidCoupon.discountValue
+      );
+
+      totalDiscounted = Number(discountPrice);
+    
+      deliveryFee = isValidCoupon.discountType === "DELIVERY" ? 0 : deliveryTax;
+    }
+
+    const data = {
+      deliveryAddress: body.deliveryAddress,
+      paymentMethod: body.paymentMethod,
+      userId: user.id,
+      totalOriginal: totalOriginal,
+      totalDiscounted: totalDiscounted,
+      deliveryFee: deliveryFee,
+    };
+
+    const newOrder = await Order.createOrder(data, cart.items);
+    res.status(201).json(newOrder);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const errorField = error.issues.map((el) => el.path.join(".")).join(", ");
+      
+      let message = `Erro de validação nos campos: ${errorField}`;
+      if (errorField.includes("deliveryAddress")) message = "Endereço obrigatório.";
+      if (errorField.includes("paymentMethod")) message = "Método de pagamento obrigatório.";
+
+      res.status(400).json({ message });
+    } else {
+      next(error);
+    }
+  }
+};
+
+export { ordersByUserId, createOrder };
