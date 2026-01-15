194b55240a9b9141466525dde4c38a281f61f0dc
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-01-15 18:22:54 -0300

    Criando o arquivo de rotas para as orders

--- src/routers/order-router.ts
+++ src/routers/order-router.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/auth-user'
-import { ordersByUserId } from '../controllers/order-controller'
+import { createOrder, ordersByUserId } from '../controllers/order-controller'
 
 const orderRouter = express.Router()
 
 orderRouter.get('/order/user/:id', authMiddleware, ordersByUserId)
+orderRouter.post('/order', authMiddleware, createOrder)
 
 export default orderRouter
