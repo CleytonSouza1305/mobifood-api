ac3203101d1a2d26a8202b41620e62ee1fbbdf0f
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-01-15 18:22:09 -0300

    Criando o método static do model Order para incluir uma nova order no banco de dados

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,24 +1,76 @@
-import { prisma } from "../database"
+import { promises } from "dns";
+import { prisma } from "../database";
+import { ProductCategory } from "../generated/prisma";
 
-type EnumStatus = 'PLACED' | 'CONFIRMED' | 'PREPARING' | 'OUT_FOR_DELIVERY' | 'DELIVERED' | 'CANCELLED'
+type EnumStatus =
+  | "PLACED"
+  | "CONFIRMED"
+  | "PREPARING"
+  | "OUT_FOR_DELIVERY"
+  | "DELIVERED"
+  | "CANCELLED";
 
 export interface CreateOrderInterface {
-  userId: number           
-  totalOriginal: number       
-  totalDiscounted: number     
-  deliveryFee?: number         
-  deliveryAddress: string      
-  paymentMethod: string      
-  status: EnumStatus          
+  userId: number;
+  totalOriginal: number;
+  totalDiscounted?: number;
+  deliveryFee?: number;
+  deliveryAddress: string;
+  paymentMethod: string;
+  status?: EnumStatus;
+}
+
+export interface cartItemArr {
+  id: number;
+  cartId: number;
+  itemId: number;
+  quantity: number;
+  subTotal: number | null;
+  item: {
+    id: number;
+    name: string;
+    category: ProductCategory;
+    price: any; 
+    restaurantId: number;
+    description: string;
+    imageUrl: string | null;
+  };
 }
 
 export class Order {
-  static createOrder = async (data: CreateOrderInterface) => {
-    return await prisma.order.create({ data })
-  }
+  static createOrder = async (
+    data: CreateOrderInterface,
+    cartProducts: cartItemArr[]
+  ) => {
+    const { userId, ...orderData } = data;
+
+    const newOrder = await prisma.order.create({
+      data: {
+        ...orderData,
+        user: {
+          connect: { id: userId },
+        },
+      },
+    });
+
+    await Promise.all(
+      cartProducts.map((product) =>
+        prisma.orderItem.create({
+          data: {
+            priceAtOrder: Number(product.item.price), 
+            productId: product.item.id,
+            orderId: newOrder.id, 
+            quantity: product.quantity,
+          },
+        })
+      )
+    );
+
+    return newOrder;
+  };
 
   static ordersByUserId = async (userId: number) => {
-    const orders = await prisma.order.findMany({ where: { userId} })
-    return orders
-  }
+    const orders = await prisma.order.findMany({ where: { userId } });
+    return orders;
+  };
 }
