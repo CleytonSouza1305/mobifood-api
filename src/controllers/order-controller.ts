90ca13b42d3566a4e564fddd0e29aea6d98b8c89
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-01-16 12:32:53 -0300

    Fazendo melhores validações para criar a order, evitando erros e seguindo a regra de negócio

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -7,6 +7,8 @@ import { ZodError } from "zod";
 import { Cart } from "../models/Cart";
 import { Coupon } from "../models/Coupon";
 import calculateDiscount from "../utils/calculateDiscount";
+import { Restaurant } from "../models/Restaurant";
+import { getHour, isRestaurantOpen } from "../utils/validateProductAndRestaurant";
 
 // /api/order/user/:id
 const ordersByUserId: Handler = async (req, res, next) => {
@@ -48,9 +50,40 @@ const createOrder: Handler = async (req, res, next) => {
     if (!cart || cart.items.length <= 0) {
       throw new HttpError(400, "Carrinho vazio ou não encontrado.");
     }
-    
+
+    const allRestaurants = await Promise.all(
+      cart.items.map(async (i) => {
+        const restaurant = await Restaurant.onlyRestaurant(
+          Number(i.item.restaurantId)
+        );
+        if (!restaurant) {
+          throw new HttpError(
+            404,
+            "Erro ao buscar restaurante, tente novamente mais tarde"
+          );
+        }
+
+        return restaurant;
+      })
+    );
+
+    const closedRestaurants = allRestaurants.filter((r) => {
+      if (
+          !isRestaurantOpen(getHour(r.openAt), getHour(r.closeAt))
+        ) {
+          return true
+        } else {
+          return false
+        }
+    });
+
+    if (closedRestaurants && closedRestaurants.length > 0) {
+      const names = [...new Set(closedRestaurants.map(r => r.name))].join(", ")
+      throw new HttpError(400, `Pedido cancelado. Os seguintes restaurantes estão fechados agora: ${names}`);
+    }
+
     let totalOriginal = cart.total;
-    let totalDiscounted = cart.total; 
+    let totalDiscounted = cart.total;
     let deliveryFee = deliveryTax;
 
     if (body.couponCode) {
@@ -67,7 +100,7 @@ const createOrder: Handler = async (req, res, next) => {
       );
 
       totalDiscounted = Number(discountPrice);
-    
+
       deliveryFee = isValidCoupon.discountType === "DELIVERY" ? 0 : deliveryTax;
     }
 
@@ -85,10 +118,12 @@ const createOrder: Handler = async (req, res, next) => {
   } catch (error) {
     if (error instanceof ZodError) {
       const errorField = error.issues.map((el) => el.path.join(".")).join(", ");
-      
+
       let message = `Erro de validação nos campos: ${errorField}`;
-      if (errorField.includes("deliveryAddress")) message = "Endereço obrigatório.";
-      if (errorField.includes("paymentMethod")) message = "Método de pagamento obrigatório.";
+      if (errorField.includes("deliveryAddress"))
+        message = "Endereço obrigatório.";
+      if (errorField.includes("paymentMethod"))
+        message = "Método de pagamento obrigatório.";
 
       res.status(400).json({ message });
     } else {
