3d0efd3343aadfc090b8e867bc497e9dc7f2a7a0
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-02-11 14:46:52 -0300

    Criando o primeiro controller para buscar os métodos de pagamento do usuário

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -1,13 +1,39 @@
 import { Handler } from "express";
+import { User } from "../models/User";
+import { HttpError } from "../error/HttpError";
+import { JwtPayload } from "jsonwebtoken";
+import { Payment } from "../models/Payment";
 
+// GET api/payment/:userId
 const paymentByUser: Handler = async (req, res, next) => {
   try {
-    const { userId } = req.params
-    const user = await 
+    const { userId } = req.params;
+    const existsUser = await User.findById(+userId);
 
+    if (!existsUser) {
+      throw new HttpError(404, "Usuário não encontrado.");
+    }
+
+    const sessionUser = req.user;
+
+    if (
+      !sessionUser ||
+      typeof sessionUser !== "object" ||
+      !("id" in sessionUser)
+    ) {
+      throw new HttpError(401, "Usuário não autenticado.");
+    }
+
+    const user = sessionUser as JwtPayload & { id: number; role: string };
+    if (user.id !== existsUser.id && user.role !== "admin") {
+      throw new HttpError(403, "User not authorized.");
+    }
+
+    const paymentsMethods = await Payment.paymentByUserId(existsUser.id)
+    res.json(paymentsMethods)
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-export { paymentByUser }
+export { paymentByUser };
