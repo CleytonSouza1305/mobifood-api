277d8cc0b8b290cde235bfba412a5b9501bfab70
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-02-11 14:47:34 -0300

    Criando o caminho para buscar os métodos de pagamento pelo id do usuário

--- src/routers/payment-router.ts
+++ src/routers/payment-router.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import { authMiddleware } from '../middleware/auth-user'
+import { paymentByUser } from '../controllers/payment-controller'
 const paymentRouter = express.Router()
 
-
+paymentRouter.get('/payment/:userId', authMiddleware, paymentByUser)
 
 export default paymentRouter
